a19116af88e47ddd774c0b00854a2a6a6683e336
Serhii Riznychuk <sergiy19981@gmail.com>
2019-04-19 09:04:23 +0200

    Update authentication.py

--- lib/static/authentication/authentication.py
+++ lib/static/authentication/authentication.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import request, Response
+from flask import Flask, request, Response
 from lib.static.template.jsons import \
     JSONVerifiedUser , \
     JSONSaveUserList , \
@@ -8,24 +8,36 @@ import random
 import json
 import string
 
+FlaskSerwer = Flask(__name__)
+
 class UserManager(object):
 
     def __init__(self):
-        self.key = self.__generate_key()
+        self.__key = self.__generate_key()
         self.__initialize_users()
-        self.__user_id = list()
+        self.__user_id = dict()
 
     def __generate_key(self, length:int, login=None):
-        k = "".join([random.choice(string.ascii_letters + string.digits) for _ in range(length)])
-        (login and self.__user_id.append((login, k)))
+        k = "".join([random.choice(string.ascii_letters + string.digits) for _ in range(length)])    
+        (login and self.__user_id[login] = k))
         return k
 
     def __error401_NotVarifyAccess(self):
-        return JSONError(401, "Not verified access to resource")
+        resp = Response(JSONError(401, "Not verified access to resource"))
+        resp.content_type = "application/json"
+        FlaskSerwer.process_response(resp)
+        return resp
+    
+    def __error401_NotCorrectLogin(self):
+        resp = Response(JSONError(401, "Login are not correct"))
+        resp.content_type = "application/json"
+        FlaskSerwer.process_response(resp)
+        return resp
 
     def __verify_key(self, key):
         for log, ukey in self.__user_id:
-            if ukey == key: return key
+            if ukey == key: #return key
+                return __generate_key(10, log)
         return False
 
     def __verify_user(self, dictionary):
@@ -42,18 +54,36 @@ class UserManager(object):
               and
              (self.__generate_key(dictionary["login"])))
         )
-
+    
+    def __verify_login(self, login, password):
+        if not (JSONVerifiedUser(self.__key, dictionary["login"], dictionary["password"])):
+            return (self.__generate_key(dictionary["login"]))
+        return False              
+             
+    
+    def __authentifiation__service__error(self):
+        resp = Response(JSONError(500, "internal server error"))
+        resp.content_type = "application/json"
+        FlaskSerwer.process_response(resp)
+        return resp
 
     def authenticate(self, f):
         @wraps(f)
         def decorator(*args, **kwargs):
             dictionary = json.dumps(request.get_json())
-            if("userloginkey" in dictionary):
-                return f(*args)
-                getKey(JSONVerifiedUser())
-            else:
-                error401_NotVarifyAccess()
-            return f(*args, **kwargs)
+            if("temporarykey" dictionary):
+                if(__verify_key):
+                    return f(key, *args, **kwargs)
+                else:
+                    return __error401_NotVarifyAccess(*args, **kwargs)
+            if("login" in dictionary and "password" in dictionary):
+                key = serlf.__verify_login(dictionary["login"], dictionary["password"])
+                if(key):
+                    return f(key, *args, **kwargs)
+                else:
+                    return __error401_NotCorrectLogin(*args, **kwargs)
+             return __authentifiation__service__error()
+                    
         return decorator()
 
 
